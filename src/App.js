d76107fec6c2b255ec721b3b9de9709c95c5175f
unknown <y.dahmouh@ecole-ipssi.net>
2023-11-02 10:57:27 +0100

    Maj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Compenent/Home';
+import Pokedex from './Compenent/Pokedex';
+import { Link, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Home />
+      <Pokedex /> */}
+      <ul className='nav'>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/pokedex'>Pokedex</Link></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pokedex' element={<Pokedex />} />
+      </Routes>
+
     </div>
   );
 }
